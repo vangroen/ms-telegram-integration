c8099a1c0d2f7ab89e3fc81119cb9a5c5b13a0a4
Pzegarra <pzegarra@proteccion.com.co>
2026-01-02 19:56:26 -0500

    feat: mejoras

--- src/data_extractor.ts
+++ src/data_extractor.ts
@@ -48,9 +48,9 @@ async function analizarVoucherConGemini(imageBuffer: Buffer): Promise<any | null
 
     const genAI = new GoogleGenerativeAI(geminiApiKey);
     // Usamos 2.0 Flash por eficiencia
-    //const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
-    //const model = genAI.getGenerativeModel({ model: "gemini-2.5-pro" });
-    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
+    const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+    // const model = genAI.getGenerativeModel({ model: "gemini-2.5-pro" });
+    // const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash-lite" });
 
     const prompt = `
         Actúa como un OCR financiero experto. Analiza la imagen.
